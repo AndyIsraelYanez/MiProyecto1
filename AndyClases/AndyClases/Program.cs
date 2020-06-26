97144c5532077ed8c25a7b08611875d2d8e50022
andy de la cruz <drag.andy@outlook.com>
2020-06-26 18:48:41 -0500

    sadasa

---
+++ AndyClases/AndyClases/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AndyClases
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World hola!");
+        }
+    }
+}
